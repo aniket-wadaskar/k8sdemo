867e66413a653f9288d9b36416a6a42b53860f97
aniket-wadaskar <arwadaskar2157@gmail.com>
2023-11-01 12:06:46 +0530

    Intial commit

--- src/test/java/com/cg/K8sDemoProjectApplicationTests.java
+++ src/test/java/com/cg/K8sDemoProjectApplicationTests.java
@@ -3,10 +3,10 @@ package com.cg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class K8sDemoProjectApplicationTests {
 
-	@Test
+	
 	void contextLoads() {
 	}
 
